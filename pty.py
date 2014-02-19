4782d6d67def002340ee43813edbdffc9ab2adc3
Daniel Evans <danharibo@gmail.com>
2014-02-19 03:41:46 +0000

    functional win state

--- pty.py
+++ pty.py
@@ -4,11 +4,11 @@ Tr = 1
 Tw = 2
 
 def gen_row(size, row = 0):
-	return [Te] * (size-1 if row%2 else size)
+	return [Te] * (size if row%2 else (size-1))
 
 def gen_board(size):
 	f = lambda x: gen_row(size, x)
-	return [f(x) for x in range((size*2)-1)]
+	return [f(x) for x in range((size*2)+1)]
 
 def dup_board(board):
 	b = []
@@ -20,6 +20,7 @@ def dup_board(board):
 	return b
 
 def make_move(board, move, token):
+	board = dup_board(board)
 	board[move[0]][move[1]] = token
 	return board
 
@@ -44,76 +45,163 @@ def print_board(board):
 	for r in board:
 		c = row_color(len(r))
 		dot = '%s·' % col_esc(c)
-		p = '%s ' % dot if len(r)%2 else ''
-		e = ' %s' % dot if len(r)%2 else ''
+		p = '%s%s ' % ('' if len(r)%2 else '  ', dot)
+		e = ' %s' % dot
 		print("%s%s%s\033[0m" % (p, (' %s ' %dot).join(map(gf(len(r)), r)), e))
 
-def move_value(board, move, token):
-	bc = dup_board(board)
-	nb = make_move(bc, move, token)
-	score = 0
-	if token == Tr:
-		for r in range(1, len(bc), 2):
-			if Tr in bc[r]:
-				score += 1
-	elif token == Tw:
-		for r in range(1, len(bc), 2):
-			if bc[r][0] == Tw:
-				score = 2
-			else:
-				rowscore = 0
-				for l in range(len(bc[r])):
-					if bc[r][l] == Tw:
-						rowscore += 1
-				score = max(score, rowscore)
-	return score
-
 # operations (row, index, 1)
-def available_operations(board, token):
+def avail_moves(board):
 	ops = []
 	for y in range(len(board)):
 		for x in range(len(board[y])):
 			if x == 0 and y == 0:
 				continue
-			if x == 0 and y == len(board)-1:
+			if x == 0 and y == len(board):
 				continue
-			if x == len(board[y])-1 and y == 0:
+			if x == len(board[y]) and y == 0:
 				continue
-			if x == len(board[y])-1 and len(board)-1:
+			if x == len(board[y]) and len(board):
 				continue
 			if board[y][x] == Te:
-				v = move_value(board, (y, x), token)
-				ops.append((y, x, v))
+				ops.append((y, x))
 	return ops
 
-def best_move(board, moves, token):
+# for red rows:
+#  red: <=>, V> ^> V ^
+#  wht : V2 ^2 V V> ^ ^>
+# for white rows:
+#  ^< ^
+#  red: V2 ^2 V V> ^ ^>
+#  wht: <=> V> ^> V ^
+def adjacent_points(p, board):
+	ps = []
+	tc = board[p[0]][p[1]]
+	if row_color(p[0]) == Tr:
+		if p[0] > 0:
+			ps.append((p[0]-1, p[1]))
+			ps.append((p[0]-1, p[1]+1))
+		if p[0] < len(board)-1:
+			ps.append((p[0]+1, p[1]))
+			ps.append((p[0]+1, p[1]+1))
+		if tc == Tr:
+			if p[1] > 0:
+				ps.append((p[0], p[1]-1))
+			if p[1] < len(board[p[0]])-1:
+				ps.append((p[0], p[1]+1))
+		elif tc == Tw:
+			if p[0] > 3:
+				ps.append((p[0]-2, p[1]))
+			if p[0] < len(board)-3:
+				ps.append((p[0]+2, p[1]))
+	if row_color(p[0]) == Tw:
+		if p[1] > 0:
+			ps.append((p[0]-1, p[1]-1))
+			ps.append((p[0]+1, p[1]-1))
+		if p[1] < len(board[p[0]])-1:
+			ps.append((p[0]-1, p[1]))
+			ps.append((p[0]+1, p[1]))
+		if tc == Tr:
+			if p[0] > 2:
+				ps.append((p[0]-2, p[1]))
+			if p[0] < len(board)-2:
+				ps.append((p[0]+2, p[1]))
+		elif tc == Tw:
+			if p[1] > 0:
+				ps.append((p[0], p[1]-1))
+			if p[1] < len(board)-1:
+				ps.append((p[0], p[1]+1))
+	return ps
+
+def point_in_set(point, sett):
+	for p in sett:
+		if point[0] == p[0] and point[1] == p[1]:
+			return True
+	return False
+
+def find_winner(board):
+	for c in range(len(board[1])):
+		sopen = [(1, c)]
+		sclosed = []
+		f = lambda x: not point_in_set(x, sclosed) and board[x[0]][x[1]] == Tr
+		while len(sopen) > 0:
+			toppe = sopen.pop(0)
+			if toppe[0] == len(board)-2:
+				return Tr
+			sclosed.append(toppe)
+			sopen.extend(filter(f, adjacent_points(toppe, board)))
+	for c in range(1, len(board), 2):
+		sopen = [(c, 0)]
+		sclosed = []
+		f = lambda x: not point_in_set(x, sclosed) and board[x[0]][x[1]] == Tw
+		while len(sopen) > 0:
+			toppe = sopen.pop(0)
+			if toppe[1] == len(board[1])-1:
+				return Tw
+			sclosed.append(toppe)
+			sopen.extend(filter(f, adjacent_points(toppe, board)))
+	for r in board:
+		for x in r:
+			if x == Te:
+				return None
+	return False
+
+def utility(board, player):
+	winner = Tr
+	if winner == player:
+		return 1
+	else:
+		return -1
+
+def move_value(board, move, player, plymax, plymin):
+	nb = make_move(board, move, player)
+	print_board(nb)
+	w = find_winner(board)
+	if w != None:
+		if w == plymax:
+			return 1
+		elif w == plymin:
+			return -1
+		else:
+			return 0
+	avail = avail_moves(nb)
+	v = None
+	if player == plymax:
+		v = -1000
+		for a in avail:
+			v = max(v, move_value(nb, a, plymin, plymax, plymin))
+	else:
+		v =  1000
+		for a in avail:
+			v = min(v, move_value(nb, a, plymax, plymax, plymin))
+	return v
+
+def best_move(board, moves, player, plymin):
 	score = 0
 	move = None
 	for m in moves:
-		if m[2] > score:
-			print(m)
-			score = m[2]
+		s = move_value(board, m, player, player, plymin)
+		if s > score:
+			score = s
 			move = m
 	return move
 
-def determine_win(board):
-	winner = None
-	reds = [True] * len(board[1])
-	for r in range(1, len(board), 2):
-		for x in range(0, len(board[r])):
-			if board[r][x] != Tr:
-				reds[x] = False
-	if True in reds:
-		return Tr
-
-board = gen_board(6)
+board = gen_board(3)
 print_board(board)
-for i in range(2):
+for i in range(0):
 	for t in [Tr, Tw]:
-		avail = available_operations(board, t)
-		best = best_move(board, avail, t)
+		avail = avail_moves(board)
 		print(avail)
+		best = best_move(board, avail, t, Tw if t == Tr else Tr)
 		make_move(board, best, t)
 		print_board(board)
-		print(determine_win(board))
 
+for y in range(1, len(board), 2):
+	board[y][1] = Tr
+print_board(board)
+print(find_winner(board))
+
+board = gen_board(3)
+for y in range(len(board[3])):
+		board[3][y] = Tw
+print_board(board)
+print(find_winner(board))
